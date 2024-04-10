93799e6cb8202005b55f9e3362b0ce46d4f538c6
hojinism <138195191+hojinism@users.noreply.github.com>
2024-04-10 10:28:39 +0900

    Title: 병원 거리 최소화하기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240410/병원 거리 최소화하기/min-of-hospital-distance.py
@@ -0,0 +1,39 @@
+def dist(p1, p2):
+    x1, y1 = p1
+    x2, y2 = p2
+    return abs(x2 - x1) + abs(y2 - y1)
+
+
+def combination(n, new_arr, c):
+    global combarr, combset
+    if len(new_arr) == n:
+        combset.append(new_arr)
+        return
+    for i in range(len(combarr)):
+        combination(n, new_arr + [combarr[i]], i+1)
+
+
+N, M = list(map(int, input().split()))
+arr = [list(map(int, input().split())) for _ in range(N)]
+lc = []
+lh = []
+for j in range(N):
+    for i in range(N):
+        if arr[j][i] == 1:
+            lh.append([i, j])
+        if arr[j][i] == 2:
+            lc.append([i, j])
+
+corr = [[dist(ph, pc) for ph in lh] for pc in lc]
+
+combset = []
+combination(M, [i for i in range(len(lc))], [], 0)
+
+mindist = 4*N*N*M
+for comb in combset:
+    distsum = 0
+    for ih in range(len(lh)):
+        distsum += min(corr[ic][ih] for ic in comb)
+    mindist = min(mindist, distsum)
+
+print(f'{mindist}')
