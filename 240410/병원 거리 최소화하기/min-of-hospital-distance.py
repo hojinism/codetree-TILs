c39f065878fcf4c0dc0783bd5bd6b60c8ce13eb4
hojinism <138195191+hojinism@users.noreply.github.com>
2024-04-10 15:01:50 +0900

    Title: 병원 거리 최소화하기, Time: 206ms, Memory: 27MB, Status: Passed - CodeTree

--- 240410/병원 거리 최소화하기/min-of-hospital-distance.py
+++ 240410/병원 거리 최소화하기/min-of-hospital-distance.py
@@ -1,58 +1,46 @@
-# 20240409 20:30 -- 22:44 / 20240410 10:15 --
-
-
-import math
-import time
-
-
-def dist(p1, p2):
-    x1, y1 = p1
-    x2, y2 = p2
-    return abs(x2 - x1) + abs(y2 - y1)
-
-
-def combination(n, new_arr, c):
-    global combarr, combset
-    if len(new_arr) == n:
-        combset.append(new_arr)
-        return
-    for i in range(c, len(combarr)):
-        combination(n, new_arr + [combarr[i]], i+1)
-
-
-# start = time.time()
-
-N, M = list(map(int, input().split()))
+N, M = map(int, input().split())
 arr = [list(map(int, input().split())) for _ in range(N)]
-lc = []
-lh = []
+
+chickens = []
+houses = []
 for j in range(N):
     for i in range(N):
-        if arr[j][i] == 1:
-            lh.append([i, j])
-        elif arr[j][i] == 2:
-            lc.append([i, j])
+        if arr[j][i] == 2:
+            chickens.append((i, j))
+        elif arr[j][i] == 1:
+            houses.append((i, j))
 
-corr = [[dist(ph, pc) for ph in lh] for pc in lc]
 
-# end = time.time()
-# print(f"{end - start:.5f} sec")
+def dist(chicken, house):
+    cx, cy = chicken
+    hx, hy = house
+    return abs(cx - hx) + abs(cy - hy)
 
-combset = []
-combarr = [i for i in range(len(lc))]
-combination(M, [], 0)
 
-# end = time.time()
-# print(f"{end - start:.5f} sec")
+# 각 house마다 가장 가까운 chicken집을 sort하여 시간을 줄일 수 있음!
+corr = [sorted(list(enumerate(dist(chicken, house) for chicken in chickens)), key=lambda x: x[1]) for house in houses]
 
-mindist = 4*N*N*M
-for comb in combset:
-    distsum = 0
-    for ih in range(len(lh)):
-        distsum += min(corr[ic][ih] for ic in comb)
-    mindist = min(mindist, distsum)
+selected = [False for _ in range(len(chickens))] # 선택되었는지 확인용
+mindist = 250000 # 적당히 큰 수
 
-# end = time.time()
-# print(f"{end - start:.5f} sec")
+def comb(cn, ci):
+    global mindist, selected
+    if cn == M:
+        tmp = 0
+        for D in corr:
+            for idx, dst in D:
+                if selected[idx]:
+                    tmp += dst
+                    break
+        mindist = min(mindist, tmp)
+        return
+    for i in range(ci, len(chickens)):
+        selected[i] = True
+        comb(cn + 1, i + 1)
+        selected[i] = False
 
-print(f'{mindist}')
+
+comb(0, 0)
+
+
+print(mindist)
